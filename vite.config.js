3b19555d5d368623211e559c98c14da7e54d4b77
gahamagar <imaganeshgaha@gmail.com>
2026-04-22 16:08:47 +0545

    Fix vite config and app logic

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/terminal-portfolio/', // Replace 'antigravity-portfolio' with your exact GitHub Repo name
+  base: '/terminal-portfolio/',
 })
