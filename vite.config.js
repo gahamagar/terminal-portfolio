b5c4402b49c5971504031524be24e36971e230c7
Ganesh Gaha <59595263+gahamagar@users.noreply.github.com>
2026-05-01 13:39:50 +0545

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/terminal-portfolio/',  // 👈 must match your repo name exactly
+  base: '/',  // 👈 must match your repo name exactly
 })
